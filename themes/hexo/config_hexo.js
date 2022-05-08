67694960eccababe17c3b3d17c0a8b1590600e2e
oceanyyy <100446500+oceanyyy@users.noreply.github.com>
2022-05-08 18:09:31 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -13,13 +13,13 @@ const CONFIG_HEXO = {
   POST_LIST_PREVIEW: true, // 读取文章预览
 
   ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  ARTICLE_RECOMMEND: true, // 文章关联推荐
+  ARTICLE_COPYRIGHT: false, // 显示文章版权声明
+  ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   WIDGET_ANALYTICS: false, // 显示统计卡
   WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: true, // 跳到评论区
+  WIDGET_TO_COMMENT: false, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
   WIDGET_TOC: true // 移动端悬浮目录
 }
