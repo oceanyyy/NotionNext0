71c441b890dbb783853ac99c102838587cc8a68a
oceanyyy <100446500+oceanyyy@users.noreply.github.com>
2022-05-08 15:08:54 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = ({ title }) => {
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br/>
         <h1>{title}</h1>
-        <span className='text-xs font-serif'>Powered by Notion & NotionNext.</span></span><br/>
+        <span className='text-xs font-serif'>Powered by Notion & NotionNext.</a>.</span></span><br/>
 
     </footer>
   )
